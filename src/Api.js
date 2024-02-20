302a1aff6ce09b5b8cd6e5f6ad13f8eeaecd9a46
Jason <murangira99@gmail.com>
2024-02-20 17:13:40 +0200

    added return jason and closing tags

--- src/Api.js
+++ src/Api.js
@@ -3,3 +3,6 @@ const getWeather = async (city) => {
     return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
     .then((res) => res.json())
     .then((json) => {
+        return json;
+    })
+}
