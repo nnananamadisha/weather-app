59a71909f69ac1f8dbbb836d7f2739e21698ca59
Jason <murangira99@gmail.com>
2024-02-20 17:14:22 +0200

    deleted a consta

--- src/Api.js
+++
@@ -1,8 +0,0 @@
-const apiKey = '3afe3263a8cfcf3ce70fe179ae5e0b0b';
-const getWeather = async (city) => {
-    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
-    .then((res) => res.json())
-    .then((json) => {
-        return json;
-    })
-}
